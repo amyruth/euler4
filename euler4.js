23d92ed2da02371fafe40d6d953228ebc656535f
amyruth <anrutherford@gmail.com>
2014-06-30 16:23:40 -0400

    Well, I tried. It was something to do last night.

--- euler4.js
+++ euler4.js
@@ -1,15 +1,22 @@
 var a = 100;
 var b = 100;
-var num = 0;
 var product;
-
+var values = [];
 for(var i=100; i<=999;i++){
   product = a * b;
   var product2 = product.toString();
   var reverse = product2.split("").reverse().join("");
   if(product2 == reverse){
-  console.log(a +"and " + b + " = " + product2);
+  	values.push(parseInt(product2));
+  	
 }
   a += 1;
   b +=1;
 }
+console.log(values);
+console.log("The largest palindrome number found is: "+Math.max.apply(Math, values));
+
+/*Just realized I'm not taking all the possibilities into account, just matched pairs of numbers. Oh well, it was something to do.
+*//*Have to pull largest palindrome from array
+figure out how to display the two multipliers
+put console msg in new postion.*/
